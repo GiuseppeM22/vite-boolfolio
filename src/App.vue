e3a09804e9188c44eb8d1f18a148dbe52643c7a9
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-07-17 16:01:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from "axios";
+
+export default {
+    name: "AppMain",
+    data() {
+        return {
+            apiUrl: "http://localhost:8000/api/prova",
+           
+        }
+    },
+    methods:{
+      getApi(){
+          axios.get(this.apiUrl).then(response => {
+                      console.log(response.data);
+          
+          })
+      }
+    },
+    mounted(){
+      this.getApi()
+    } 
+}
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+</style>
