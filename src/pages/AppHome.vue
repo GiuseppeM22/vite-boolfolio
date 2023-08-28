557f3599d64e28b8589b979d30b462b5e1dc0a97
“GiuseppeM22” <giu.mongelli97@gmail.com>
2023-08-28 15:24:42 +0200

    add link back end

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -2,4 +2,5 @@
 <template>
     <h1>Welcome home</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod molestiae, velit cumque ab nam dicta quaerat amet veritatis deserunt praesentium, totam debitis quae a inventore repellendus, officia laborum perspiciatis ad.</p>
+    <a href="http://localhost:8000/">vai al back-end</a>
 </template>
